2f8d0d2b9780572084023a9f9bda9a8588a88f96
Lizzy Mitchell <burnlikethesun@gmail.com>
2019-08-29 22:03:59 +0100

    Generate button now processes password

--- BankingPassword/MainWindow.xaml.cs
+++ BankingPassword/MainWindow.xaml.cs
@@ -45,5 +45,29 @@ namespace BankingPassword
             Regex regex = new Regex("[^0-9]+");
             e.Handled = regex.IsMatch(e.Text);
         }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string password = passwordBox.Password;
+            string index1Text = index1TextBox.Text;
+            string index2Text = index2TextBox.Text;
+            string index3Text = index3TextBox.Text;
+
+            int index1;            
+            int index2;            
+            int index3;
+
+            int.TryParse(index1Text, out index1);
+            int.TryParse(index2Text, out index2);
+            int.TryParse(index3Text, out index3);
+
+            if (index1 > 0 && index2 > 0 && index3 > 0 && !string.IsNullOrEmpty(password))
+            {
+                result1TextBox.Text = password.Substring(index1 - 1, 1);
+                result2TextBox.Text = password.Substring(index2 - 1, 1);
+                result3TextBox.Text = password.Substring(index3 - 1, 1);
+            }
+
+        }
     }
 }
